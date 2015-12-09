e659788b8dcdc6355563187ddb897628b288a6e4
Mark-A-A <markawai01@gmail.com>
2015-12-08 21:42:23 -0500

    added buttons and started the functionality

--- js/game.js
+++ js/game.js
@@ -1,6 +1,44 @@
+window.game = {
+  userCount: 0,
+  computerCount: 0,
+  round: 1,
+};
+
 $(document).ready(function(){
-  var count;
   var rockPaperScissors =["rock", "paper", "scissors"];
-  console.log(rockPaperScissors);
+    console.log(rockPaperScissors);
+  var userChoice;
+  $("#rock").on("click", function(){
+    userChoice = "rock";
+  }); 
+  $("#paper").on("click", function(){
+    userChoice = "paper";
+  }); 
+  $("#scissors").on("click", function(){
+    userChoice = "scissors";
+  }); 
+
+
+
+  $("#play").on("click", function(e) {
+    e.preventDefault();
+    alert("It works!");
+    
+    var computerChoice = Math.floor(Math.random()*(rockPaperScissors.length));
+      console.log(computerChoice); //Testing my variable for computer's choice
+    
+      
+    // Add logic here
+    if (){
+
+    }else if(){
+
+    }else {
+
+    };
   
+  });
+
+
+
 }); //end of document function
