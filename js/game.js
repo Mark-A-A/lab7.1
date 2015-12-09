d6024df158607a08b06a7014f336e3538320d854
Mark-A-A <markawai01@gmail.com>
2015-12-09 15:29:31 -0500

    modified play function to userChoice of rock paper scissors buttons; need comparison with computerChoice

--- js/game.js
+++ js/game.js
@@ -7,28 +7,38 @@ window.game = {
 $(document).ready(function(){
   var rockPaperScissors =["rock", "paper", "scissors"];
     console.log(rockPaperScissors);
-  var userChoice;
+  var userChoice =0 ;
   $("#rock").on("click", function(){
+    //alert("you picked rock!");
     userChoice = "rock";
+    //console.log(userChoice);
+
   }); 
+
   $("#paper").on("click", function(){
+    //alert("you picked paper!");
     userChoice = "paper";
-  }); 
+    //console.log(userChoice);
+    
+  });
+
   $("#scissors").on("click", function(){
+    //alert("you picked scissors!");
     userChoice = "scissors";
+    //console.log(userChoice);
   }); 
 
 
 
-  $("#play").on("click", function(e) {
+  $(".play").on("click", function(e) {
     e.preventDefault();
-    alert("It works!");
+    alert("It works! You picked "+ userChoice);
     
     var computerChoice = Math.floor(Math.random()*(rockPaperScissors.length));
       console.log(computerChoice); //Testing my variable for computer's choice
-    
+      console.log(userChoice);
       
-    // Add logic here
+    /* Add logic here
     if (){
 
     }else if(){
@@ -36,7 +46,7 @@ $(document).ready(function(){
     }else {
 
     };
-  
+  */
   });
 
 
