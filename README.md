f83b17b2fea0045cb39cc76179afedf72cf9890b
Mark-A-A <markawai01@gmail.com>
2015-12-14 16:04:08 -0500

    started working with switching alerts with modal, Added Project Descripton to ReadME

--- README.md
+++ README.md
@@ -4,3 +4,26 @@ Objectives:
 2. Create counters for user / enemy scores and rounds
 3. Use conditional logic taking user input and determining who wins -- the user or the computer
 4. Use a bootstrap modal in place of an alert or prompt
+
+Homework:
+Create a Rock, Paper Scissors game with jQuery. Must be deployed on Heroku and pushed to GitHub.
+The game should:
+-Have the computer opponent make a play
+-Determine who won the round
+-Keep a running tally of round wins and losses
+-Stop after 5 rounds
+-Declare a winner of the game
+-Allow the user the gracefully exit or play again.
+-Use a Bootstrap modal to display information.
+
+Optional:
+-Allow the user to choose number of rounds, odd number only.
+or
+Allow the user to choose the number of rounds, any number. (Must prepare to declare a tie.)
+
+Optional II:
+-Research and Include the additional logic for (Rock, Paper, Scissors, Lizard, Spock).
+ 
+ 
+ 
+Deliverable: Github replant & deployed Heroku app.
--- js/game.js
+++ js/game.js
@@ -2,12 +2,16 @@ window.game = {
   userCount: 0,
   computerCount: 0,
   round: 1,
+
 };
 
 $(document).ready(function(){
   var rockPaperScissors =["rock", "paper", "scissors"];
-    console.log(rockPaperScissors);
-  var userChoice =0 ;
+  var userChoice =0 ;  
+  var computerChoice=0;
+
+  console.log(rockPaperScissors);
+  
   $("#rock").on("click", function(){
     //alert("you picked rock!");
     userChoice = "rock";
@@ -34,7 +38,7 @@ $(document).ready(function(){
     e.preventDefault();
     alert("You picked "+ userChoice);
     
-    var computerChoice = Math.floor(Math.random()*(rockPaperScissors.length));
+    computerChoice = Math.floor(Math.random()*(rockPaperScissors.length));
     console.log(computerChoice); //Testing my variable for computer's choice
       
     if (userChoice==="rock"){
@@ -44,32 +48,66 @@ $(document).ready(function(){
     } else {
       userChoice = 2;
     };
+  
 
     console.log(userChoice);     //Testing my variable for user's Choice
     
 
-    /* Add logic here
+    //Add logic here
     
 
-    rock = 0
-    paper = 1
-    scissors =2
+    // rock = 0
+    // paper = 1
+    // scissors =2
 
     if (userChoice === 0  && computerChoice === 1){ // 
-
+      console.log("user picked rock, computer picked paper");
+      game.computerCount++;
+      alert("User was smothered by Computer's paper. The score is: " + game.userCount + " | " + game.computerCount);
+      $("#comp-score").html(game.computerCount);
+      
     }else if(userChoice === 0 && computerChoice === 2 ){
+      console.log("user picked rock, computer picked scissors.");
+      game.userCount++;
+      alert("user smashed scissors. The score is: " + game.userCount + " | " + game.computerCount);
+      $("#your-score").html(game.userCount);
 
-    }else if(userChoice === 1 && computerChoice = 0){
-    
-    }else if(userChoice === 1 && computerChoice = 2){
-    
+    }else if(userChoice === 1 && computerChoice === 0){
+      console.log("user picked paper, computer picked rock");
+      game.userCount++;
+      alert("User covered rockk. The score is: " + game.userCount + " | " + game.computerCount);
+      $("#your-score").html(game.userCount);
+
+    }else if(userChoice === 1 && computerChoice === 2){
+      console.log("user picked paper, computer picked scissors.");
+      game.computerCount++;
+      alert("User was cut up by Computer's scissors. The score is: " + game.userCount + " | " + game.computerCount);
+      $("#comp-score").html(game.computerCount);
+
     }else if(userChoice === 2 && computerChoice === 0){
-    }else if(userChoice === 2 && computerChoice === 1){
+      console.log("user picked scissors, computer picked rock.");
+      game.computerCount++;
+      alert("Computer smashed user's scissors. The score is: " + game.userCount + " | " + game.computerCount);
+      $("#comp-score").html(game.computerCount);
 
-    } else if ( userChoice === computerChoice)
-      alert("you tied");
+    }else if(userChoice === 2 && computerChoice === 1){
+      console.log("user picked scissors, computer picked paper");
+      game.userCount++;
+      alert("User shredded computer's paper. The score is: " + game.userCount + " | " + game.computerCount);
+      $("#your-score").html(game.userCount);
+
+    } else if ( userChoice === computerChoice) {
+      console.log("you both picked the same thing.");
+      alert("you tied.  The score is: " + game.userCount + " | " + game.computerCount);
     };
-  */
+
+    //Game Winner
+    if (game.userCount === 3){
+      alert("You Win");
+    } else if(game.computerCount === 3) {
+      alert("computer wins- try again");
+    };
+
   });
 
 
